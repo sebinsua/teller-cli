0487d3558c2b88858222da48f3d02af43cadf07d
Seb Insua <me@sebinsua.com>
2016-01-28 15:14:14 +0000

    test(inform): test get_account_balance

--- src/api/inform/get_account_balance.rs
+++ src/api/inform/get_account_balance.rs
@@ -11,3 +11,26 @@ impl<'a> GetAccountBalance for TellerClient<'a> {
         self.get_account(&account_id).map(to_money)
     }
 }
+
+#[cfg(test)]
+mod tests {
+
+    use api::client::TellerClient;
+    use super::GetAccountBalance;
+
+    use hyper;
+    mock_connector!(GetAccountRequest {
+        "https://api.teller.io" => include_str!("../mocks/get-account.http")
+    });
+
+    #[test]
+    fn can_get_account_balance() {
+        let c = hyper::client::Client::with_connector(GetAccountRequest::default());
+        let teller = TellerClient::new_with_hyper_client("fake-auth-token", c);
+
+        let money = teller.get_account_balance("123").unwrap();
+
+        assert_eq!("1000.00 GBP", money.get_balance_for_display(&false));
+    }
+
+}
